53e2683b33ab744bf04adf524a37fc7c819c5227
Jeremy Gonzalez <jeremygonzalezg@gmail.com>
2025-03-30 23:17:28 -0400

    config prod tercero

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :logger, level: :info
 
 config :dengutech, Dengutech.Repo,
   url: System.get_env("postgresql://dengutech_dev_user:3WyJhG1P6k91lNCRdz6TS0DoIf7ThyiB@dpg-cvkvndgdl3ps738dp7ug-a.oregon-postgres.render.com/dengutech_dev"),
-  pool_size: 10
-
+  pool_size: 10,  # Ajusta según tus necesidades
+  ssl: true
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
